c2b9c5d8ba834cf2ebb89c808b8fa45a3c84ed4f
Conejosalvaje123 <angelvazquezoriginal@gmail.com>
2024-11-07 20:51:49 -0600

    secciones comentadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/components/Navbar";
-import Calendar from "@/sections/calendar/page";
-import FAQs from "@/sections/faqs/page";
+// import Calendar from "@/sections/calendar/page";
+// import FAQs from "@/sections/faqs/page";
 import Hero from "@/sections/hero/page";
 import History from "@/sections/history/page";
-import Hotel from "@/sections/hotel/page";
-import Itinerary from "@/sections/itinerary/page";
+// import Hotel from "@/sections/hotel/page";
+// import Itinerary from "@/sections/itinerary/page";
 import RSVP from "@/sections/RSVP/page";
-import ToDos from "@/sections/to-do's/page";
-import Travel from "@/sections/travel/page";
+// import ToDos from "@/sections/to-do's/page";
+// import Travel from "@/sections/travel/page";
 
 export default function Home() {
   return (
